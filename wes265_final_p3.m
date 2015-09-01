a5b4307cfa4087df47790a24ddda9bae8141be08
Joshua Emele <jemele@acm.org>
2015-08-31 17:43:41 -0700

    p3: simplify and cleanup

--- wes265_final_p3.m
+++ wes265_final_p3.m
@@ -10,22 +10,17 @@ close all;
 rp = .1
 % stopband attenuation (dB)
 rs = 100
-% convert from dB to volt
-dev = [(10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)];
-% the transition and stop band, in kHz
-f = [2.5 17.5]
-a = [1 0]
-% the sample rate, in kHz
-fs = 1280
+fo = [0 2.5 17.5 640]/640
 bins = 4096
 
-% Estimate the filter order using firpmord and the Harris approximation.
-% Use the the worst-case value and generate the filter.
-% We ignore the (large) w value created, and use our own empirically derived
-% value.
-[n,fo,ao,w]=firpmord(f,a,dev,fs)
-n = max(ceil((fs/(f(2)-f(1)))*(rs/22)),n)
-h = firpm(447,fo,ao,[1 4]);
+% The Harris approximation yields N=389, but since we know we are going to
+% upsample by 64, we choose the next largest filter with order divisible by 64. 
+% The w value was empirically determined and appears to be the smallest
+% admissisble value.
+N = 447
+ao = {'myfrf', [1 1 0 0]}
+w = [1 2]
+h = firpm(N,fo,ao,w);
 
 % 3a. Show the time series and spectrum of the prototype filter. Show the
 % filter specifications on the spectrum.
@@ -49,11 +44,11 @@ xlabel('Normalized Frequency');
 ylabel('Log Magnitude (dB)');
 
 % plot the filter specifications
-line('XData',[-0.5 0.5],'YData',[-rs -rs], 'LineWidth', 1, 'Color','r')
-line('XData',[-2*f(1)/fs -2*f(1)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[+2*f(1)/fs +2*f(1)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[-2*f(2)/fs -2*f(2)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[+2*f(2)/fs +2*f(2)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
+line('XData',[-0.5 0.5],'YData',[-rs -rs], 'Color','r')
+line('XData',[-fo(2) -fo(2)],'YData',ylim, 'Color','r')
+line('XData',[+fo(2) +fo(2)],'YData',ylim, 'Color','r')
+line('XData',[-fo(3) -fo(3)],'YData',ylim, 'Color','r')
+line('XData',[+fo(3) +fo(3)],'YData',ylim, 'Color','r')
 
 subplot(3,2,5);
 hold on;
@@ -64,10 +59,6 @@ title('Zoom to Passband Ripple');
 xlabel('Normalized Frequency');
 ylabel('Log Magnitude (dB)');
 
-% These aren't relevent, since there's almost no passband ripple.
-line('XData',[-0.5 0.5],'YData',[+rp/2 +rp/2], 'LineWidth', 1, 'Color','r')
-line('XData',[-0.5 0.5],'YData',[-rp/2 -rp/2], 'LineWidth', 1, 'Color','r')
-
 subplot(3,2,6);
 hold on;
 plot((-0.5:1/bins:0.5-1/bins), frequency_response);
@@ -76,11 +67,11 @@ axis([fo(3)/2-0.01 fo(3)/2+0.05 -rs-5 -rs+5]);
 title('Zoom to Stopband Ripple');
 xlabel('Normalized Frequency');
 ylabel('Log Magnitude (dB)');
-line('XData',[-0.5 0.5],'YData',[-rs -rs], 'LineWidth', 1, 'Color','r')
-line('XData',[-2*f(1)/fs -2*f(1)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[+2*f(1)/fs +2*f(1)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[-2*f(2)/fs -2*f(2)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
-line('XData',[+2*f(2)/fs +2*f(2)/fs],'YData',ylim, 'LineWidth', 1, 'Color','r')
+line('XData',[-0.5 0.5],'YData',[-rs -rs], 'Color','r')
+line('XData',[-fo(2) -fo(2)],'YData',ylim, 'Color','r')
+line('XData',[+fo(2) +fo(2)],'YData',ylim, 'Color','r')
+line('XData',[-fo(3) -fo(3)],'YData',ylim, 'Color','r')
+line('XData',[+fo(3) +fo(3)],'YData',ylim, 'Color','r')
 
 % 3b. Show the time series and spectrum pf the derivative filter dh(n) formed
 % by convolution with [1 0 -1]/2 phase aligned with h(n) by discarding end
